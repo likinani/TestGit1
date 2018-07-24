fd5903035c1e74bfc348fe3101e123849c4997cb
likinani <nithya4a2@gmail.com>
2018-07-24 19:32:53 +0530

    user orgm

--- org/cts/TestPrgm.java
+++ org/cts/TestPrgm.java
@@ -1,5 +1,12 @@
 package org.cts;
 
 public class TestPrgm {
-
+	public void test() {
+		System.out.println("test");
+	}
+public static void main(String[] args) {
+	TestPrgm t=new TestPrgm();
+	t.test();
+	
+}
 }
--- org/cts/TestPrgm2.java
+++ org/cts/TestPrgm2.java
@@ -1,5 +1,13 @@
 package org.cts;
 
 public class TestPrgm2 {
+	public void test2(){
+		System.out.println("test2");
+	}
+	public static void main(String[] args) {
+		TestPrgm2 t1=new TestPrgm2();
+		t1.test2();
+		
+	}
 
 }
